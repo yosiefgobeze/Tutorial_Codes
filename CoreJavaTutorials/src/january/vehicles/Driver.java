57a188bd9efa998fa187c509717b5d1d564829f3
Yosief Gobeze <yosiefgobeze@gmail.com>
2021-01-30 09:00:05 -0600

    Added class Driver.java

---
+++ CoreJavaTutorials/src/january/vehicles/Driver.java
@@ -0,0 +1,24 @@
+package january.vehicles;
+
+/**
+ * 
+ * @author yosief
+ *
+ */
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		System.out.println("In driver main method!");
+		// method call
+		promptUser();
+	}
+	
+	/**
+	 * Will prompt the user
+	 */
+	public static void promptUser() {
+		System.out.println("In promptUser method!");
+	}
+
+}
