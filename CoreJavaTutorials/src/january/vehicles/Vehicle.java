1db02828f429fbe09f795ae363c947f088d39739
Yosief Gobeze <yosiefgobeze@gmail.com>
2021-01-30 09:02:53 -0600

    Added class Vehicle.java

---
+++ CoreJavaTutorials/src/january/vehicles/Vehicle.java
@@ -0,0 +1,31 @@
+package january.vehicles;
+
+public class Vehicle {
+	
+	// attributes for the class Vehicle
+	private int odometer;
+	private int millesPerGallon;
+	private double gallonsGas;
+	
+	// Getter and Setter methods for the attributes
+	public int getOdometer() {
+		return odometer;
+	}
+	public void setOdometer(int odometer) {
+		this.odometer = odometer;
+	}
+	public int getMillesPerGallon() {
+		return millesPerGallon;
+	}
+	public void setMillesPerGallon(int millesPerGallon) {
+		this.millesPerGallon = millesPerGallon;
+	}
+	public double getGallonsGas() {
+		return gallonsGas;
+	}
+	public void setGallonsGas(double gallonsGas) {
+		this.gallonsGas = gallonsGas;
+	}
+	
+	
+}
